d71d35a345cca5c46b78f6345c123c14c298d030
Thomas Parsons <thomascparsons@gmail.com>
2024-11-22 13:20:42 +0000

    added diffc
    diffc is the diff command but rather than returning the lines that are different it returns the total number of different lines

---
+++ src/diffc.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main(){
+	args := os.Args
+	if len(args) != 3{
+		fmt.Println("correct usage: diff <file name> <file name>")
+		return
+	}
+	file1, err := os.Open(args[1])
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	defer file1.Close()
+	file2, err := os.Open(args[2])
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	defer file2.Close()
+	scanner1 := bufio.NewScanner(file1)
+	scanner2 := bufio.NewScanner(file2)
+	linecount := 0
+	for scanner1.Scan() {
+		scanner2.Scan()
+		if scanner1.Text() != scanner2.Text(){
+			linecount++
+		}
+    }
+	fmt.Println(linecount, "lines differ")
+}
