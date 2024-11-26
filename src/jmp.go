ce4c432680698be1dd2878c9c328b82ccb9bd053
Thomas Parsons <thomascparsons@gmail.com>
2024-11-26 11:20:21 +0000

    work on jmp

--- src/jmp.go
+++ src/jmp.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+type dirName struct {
+	Name string `json:"name"`
+	Dir  string `json:"dir"`
+}
 
 func main() {
 	rootCmd := &cobra.Command{
@@ -18,7 +25,7 @@ func main() {
 		Use:   "help",
 		Short: "Help command",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("usage: jmp save <dir> / jmp to <dir>")
+			fmt.Println("usage: jmp save <name> / jmp to <dir>")
 		},
 	}
 
@@ -26,8 +33,29 @@ func main() {
 		Use:   "save",
 		Short: "Save command",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("usage: jmp save <dir>")
-			fmt.Println("save dir as json in jmp.json")
+			if len(args) != 1 {
+				fmt.Println("usage: jmp save <name>")
+				return
+			}
+			name := args[0]
+			file, err := os.Open("jmp.json")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+
+			decoder := json.NewDecoder(file)
+			data := []dirName{}
+			err = decoder.Decode(&data)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			dir, _ := os.Getwd()
+			entry := map[string]interface{}{"name": name, "dir": dir}
+			data = append(dirName, entry)
+			fmt.Println("data is", data)
 		},
 	}
 
